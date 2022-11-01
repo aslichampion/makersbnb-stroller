601db1a4cffb28ccaf2187d614dec22721a025bf
Asli Champion <aslichampion@icloud.com>
2022-11-01 14:00:46 +0000

    added sinatra files

--- app.rb
+++ app.rb
@@ -1,6 +1,15 @@
-#require 'sinatra'
-#require "sinatra/reloader"
+require 'sinatra'
+require "sinatra/reloader"
 require_relative 'lib/database_connection'
 require_relative 'lib/space_repository'
 
 DatabaseConnection.connect
+
+
+class Application < Sinatra::Base
+    # This allows the app code to refresh
+    # without having to restart the server.
+    configure :development do
+      register Sinatra::Reloader
+    end
+  end
