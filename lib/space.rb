42b33754417d5659ec418e77f2f17a4d018de749
hassanatoladini <hass09@hotmail.co.uk>
2022-11-02 14:33:04 +0000

    updated database to include more details

--- lib/space.rb
+++ lib/space.rb
@@ -1,5 +1,5 @@
 class Space
 
-    attr_accessor :id, :name 
+    attr_accessor :id, :name, :description, :photo_url, :price_per_night
 
 end
--- lib/space_repository.rb
+++ lib/space_repository.rb
@@ -4,13 +4,17 @@ class SpaceRepository
     def all
         spaces = []
 
-        sql = 'SELECT id, name FROM spaces;'
+        sql = 'SELECT id, name, description, photo_url, price_per_night FROM spaces;'
         result_set = DatabaseConnection.exec_params(sql, [])
 
         result_set.each do |record|
             space = Space.new
             space.id = record['id'].to_i
             space.name = record['name']
+            space.description = record['description']
+            space.photo_url = record['photo_url']
+            space.price_per_night = record['price_per_night']
+
             spaces << space
         end
         return spaces
@@ -18,8 +22,8 @@ class SpaceRepository
 
     def create(space)
 
-        sql = 'INSERT INTO spaces (name) VALUES ($1);'
-        result_set = DatabaseConnection.exec_params(sql, [space.name])
+        sql = 'INSERT INTO spaces (name, description, photo_url, price_per_night) VALUES ($1, $2, $3, $4);'
+        result_set = DatabaseConnection.exec_params(sql, [space.name, space.description, space.photo_url, space.price_per_night])
         
         return space
 
